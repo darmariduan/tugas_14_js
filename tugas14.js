534ae884b7bcd6ba04e6ba8b887fc6d51df2463c
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-19 22:47:08 +0700

    Tugas 14 Js

---
+++ tugas14.js
@@ -0,0 +1,7 @@
+var u = 6;
+var x = 37/u;
+
+console.log(x); //Hasil
+console.log(Math.ceil(x)); //Pembulatan ke atas
+console.log(Math.floor(x)); //Pembulatan ke bawah
+console.log(Math.round(x)); //Pembulatan acuan 0.5
